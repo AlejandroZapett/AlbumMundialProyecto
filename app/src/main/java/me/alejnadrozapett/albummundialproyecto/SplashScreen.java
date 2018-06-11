7bd8a1417cb3cd15bc0677be3f69787ed06bb173
SAID <said_s027@hotmail.es>
2018-06-10 21:01:59 -0500

    Agrego splashScreen corregida

--- app/src/main/java/me/alejnadrozapett/albummundialproyecto/SplashScreen.java
+++ app/src/main/java/me/alejnadrozapett/albummundialproyecto/SplashScreen.java
@@ -1,16 +1,41 @@
 package me.alejnadrozapett.albummundialproyecto;
 
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_splash_screen);
+        iv = (ImageView) findViewById(R.id.iv);
+
+        Animation anim = AnimationUtils.loadAnimation(this,R.anim.transition );
+        iv.startAnimation(anim);
+        final Intent i = new Intent(this,MainActivity.class);
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+
+                }
+            }
+        };
+                timer.start();
     }
 }
